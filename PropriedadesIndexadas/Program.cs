e78f11b1f2b5b8d0a06b9eb96b2a84aedc620b47
Rafael Vasconcellos <rafael.vasconcellos@accenture.com>
2019-05-24 11:13:25 -0300

    Criando o projeto

---
+++ PropriedadesIndexadas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PropriedadesIndexadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cinemark = new Cinema();
+            var rafael = new Cliente("Rafael");            
+            var vanessa = new Cliente("Vanessa");
+            var isabela = new Cliente("Isabela");
+
+            cinemark["M5"] = rafael;
+            cinemark["M6"] = vanessa;
+            cinemark["M6"] = isabela;
+
+            cinemark.ImprimirReservas();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Cinema
+    {
+        public string Assento { get; set; }
+        public Cliente Cliente { get; set; }
+
+        private readonly IDictionary<string, Cliente> Reservas = new Dictionary<string, Cliente>();
+
+        public Cliente this[string assento]
+        {
+            get
+            {
+                return Reservas[assento];
+            }
+            set
+            {
+                Reservas[assento] = value;
+            }
+        }
+
+        public void ImprimirReservas()
+        {
+            Console.WriteLine("Assentos reservados:");
+            foreach (var item in Reservas)
+            {
+                Console.WriteLine($"Assento -> {item.Key} - Cliente -> {item.Value.Nome}");
+            }
+        }
+    }
+
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
